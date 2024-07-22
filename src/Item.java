df8374ff510e72ab5f03234bbfd2bd131dcd98ca
MarioImpicciche92 <mario.impicciche@gmail.com>
2024-07-22 19:01:45 +0200

    creation Person class and Item array

---
+++ src/Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    String name;
+    double value;
+    
+    public String getName() {
+        return name;
+    }
+    public double getValue() {
+        return value;
+    }
+    
+}
