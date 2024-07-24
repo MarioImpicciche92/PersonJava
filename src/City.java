d5f532ac539fa25b209c24f9cec1ff7fdf5fe3dd
Impicciche <giuseppe.impicciche87@gmail.com>
2024-07-24 12:16:56 +0200

    Corrected errors in City class

--- src/City.java
+++ src/City.java
@@ -1,15 +1,11 @@
 public class City {
-
-    
-
     Building[] building = new Building[1];
 
     public City(){
-
-         Item[] bankItems = new Item[2];
-        bankItems[0]=new Item(name="Letter Opener",value="2.5"); // Items to control 
-    bankItems[1] = new Item(name="Stamp",value="2.5");
-    buildings[0] = new Building("Bank", bankItems);
+        Item[] bankItems = new Item[2];
+        bankItems[0] = new Item("Letter Opener",2.5); // Items to control 
+        bankItems[1] = new Item("Stamp",2.5);
+        building[0] = new Building("Bank", bankItems);
     }
     
     
--- src/Item.java
+++ src/Item.java
@@ -1,6 +1,11 @@
 public class Item {
     String name;
     double value;
+
+    public Item(String name, double value){
+        this.name = name;
+        this.value = value;
+    }
     
     public String getName() {
         return name;
